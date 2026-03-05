3e4411c7d147618668dd0ca5be4eed520724cccd
Cindy Williams Urena <cindymwu29@gmail.com>
2026-03-04 23:15:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,73 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+function saveTasks(){
+localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function renderTasks(){
+
+let list = document.getElementById("taskList")
+
+list.innerHTML=""
+
+tasks.forEach((task,index)=>{
+
+let li = document.createElement("li")
+
+if(task.completed){
+li.classList.add("completed")
+}
+
+li.innerHTML = `
+<span onclick="toggleTask(${index})">${task.text}</span>
+<button class="delete-btn" onclick="deleteTask(${index})">X</button>
+`
+
+list.appendChild(li)
+
+})
+
+}
+
 function addTask(){
 
 let input = document.getElementById("taskInput")
 
-let taskText = input.value
+let text = input.value.trim()
 
-if(taskText === "") return
+if(text==="") return
 
-let task = document.createElement("div")
+tasks.push({
+text:text,
+completed:false
+})
 
-task.className = "task"
+saveTasks()
 
-task.innerHTML = taskText
-
-document.getElementById("taskList").appendChild(task)
+renderTasks()
 
 input.value=""
 
 }
+
+function deleteTask(index){
+
+tasks.splice(index,1)
+
+saveTasks()
+
+renderTasks()
+
+}
+
+function toggleTask(index){
+
+tasks[index].completed = !tasks[index].completed
+
+saveTasks()
+
+renderTasks()
+
+}
+
+renderTasks()
